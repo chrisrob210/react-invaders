6327f2793531d7d0148517e5ce071d90ca761f23
Christopher Robinson <chriserino210@protonmail.com>
2026-01-10 21:23:03 -0600

    Add UIManager methods for updating and drawing with keyboard input handling

--- src/components/engine/UIManager.ts
+++ src/components/engine/UIManager.ts
@@ -1,3 +1,12 @@
+import { KeyboardInput } from "../../controls/KeyboardInput";
+
 export class UIManager {
 
+    update(delta: number, input: KeyboardInput) {
+        //
+    }
+
+    draw(ctx: CanvasRenderingContext2D) {
+        //
+    }
 }
